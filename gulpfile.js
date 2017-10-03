97959c85465823f9a784222c7a7cffe469d76bbe
skate <ilagnev@gmail.com>
2017-10-03 14:27:59 +0300

    add server task, node relaunch everytime when files changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+let node;
 
 gulp.task('copy-socket', () => {
 	gulp.src('node_modules/socket.io-client/dist/socket.io.js*')
 		.pipe(gulp.dest('./public/'));	
 });
 
+gulp.task('server', () => {
+
+	gulp.watch(['./server.js', './lib/**/*.js'], function() {
+		if (node) node.kill()
+			node = spawn('node', ['server.js'], {stdio: 'inherit'})
+		node.on('close', function (code) {
+			if (code === 8) {
+				gulp.log('Error detected, waiting for changes...');
+			}
+		});
+	})
+});
+
 gulp.task('default', ['copy-socket']);
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+    if (node) node.kill()
+});
