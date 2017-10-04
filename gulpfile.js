f2763ff580392a66facc9c1905c7e24229cc5713
skate <ilagnev@gmail.com>
2017-10-04 09:32:45 +0300

    fix server watch task

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,20 @@ gulp.task('server', () => {
 	gulp.watch(['./server.js', './lib/**/*.js'], function() {
 		if (node) node.kill()
 			node = spawn('node', ['server.js'], {stdio: 'inherit'})
+		
 		node.on('close', function (code) {
 			if (code === 8) {
 				gulp.log('Error detected, waiting for changes...');
 			}
 		});
 	})
+
+	// clean up if an error goes unhandled.
+	process.on('exit', function() {
+		if (node) node.kill()
+	});
+	node = spawn('node', ['server.js'], {stdio: 'inherit'})
 });
 
 gulp.task('default', ['copy-socket']);
 
-// clean up if an error goes unhandled.
-process.on('exit', function() {
-    if (node) node.kill()
-});
